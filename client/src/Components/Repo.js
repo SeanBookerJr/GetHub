2871b19f01ab20a297945a1055fe001d49de2dd5
Nathaniel Davydov <nathanielmdavydov@gmail.com>
2022-08-23 16:55:32 -0400

    Repo page done

--- client/src/Components/Repo.js
+++ client/src/Components/Repo.js
@@ -1,11 +1,56 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 
 
 function Repo() {
+
+    let navigate = useNavigate()
+
+    function handleReturn(e) {
+        navigate('/Profile')
+    }
+
     return (
         <div id="repo-container">
-            
+                    <button className="return-profile" onClick={handleReturn}>Return to Profile</button>
+
+            <h1>Repo Title PlaceHolder</h1>
+
+            <div id="repo-header">
+                <h2>Your Name PlaceHolder</h2>
+            </div>
+            <div id="file-name">
+
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <div id="file-name">
+                
+            </div>
+            <button className="live-demo">Live Demo</button>
+            <button className="add-file">Add File</button>
+            <div id="live-demo">
+                <h2>We put functionality to show live preview here</h2>
+            </div>
 
         </div>
     )
